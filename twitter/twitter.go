14b578822014da6e678ed5a1bfc98d587d7928ef
Sweetlie <sweetlie@wipeinc.io>
2018-06-03 18:35:19 +0200

    fix twitter limit

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -2,7 +2,10 @@ package twitter
 
 import (
 	"context"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	twitterGo "github.com/dghubble/go-twitter/twitter"
 	"golang.org/x/oauth2/clientcredentials"
@@ -13,12 +16,53 @@ type Client struct {
 	client *twitterGo.Client
 }
 
+type Limit struct {
+	Remaining int
+	Limit     int
+	Reset     int64
+}
+
 var credentials *clientcredentials.Config
 var accessToken string
 
 func init() {
 	accessToken = os.Getenv("TWITTER_ACCESS_TOKEN")
 	credentials = newCredentials()
+}
+
+func (l *Limit) TimeLeft() time.Duration {
+	reset := time.Unix(l.Reset, 0)
+	return reset.Sub(time.Now())
+}
+
+func (l *Limit) Delay() time.Duration {
+	return time.Duration(float64(l.TimeLeft()) / (float64(l.Remaining) + 1))
+}
+
+func GetLimits(resp *http.Response) (*Limit, error) {
+	remainingStr := resp.Header.Get("x-rate-limit-remaining")
+	remaining, err := strconv.Atoi(remainingStr)
+	if err != nil {
+		return nil, err
+	}
+
+	limitStr := resp.Header.Get("x-rate-limit-limit")
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		return nil, err
+	}
+
+	resetStr := resp.Header.Get("x-rate-limit-reset")
+	reset, err := strconv.ParseInt(resetStr, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Limit{
+		Limit:     limit,
+		Remaining: remaining,
+		Reset:     reset,
+	}, nil
 }
 
 func newCredentials() *clientcredentials.Config {
@@ -44,4 +88,18 @@ func (c *Client) GetUserShow(screenName string) (*twitterGo.User, error) {
 	userShowParams := &twitterGo.UserShowParams{ScreenName: screenName}
 	user, _, err := c.client.Users.Show(userShowParams)
 	return user, err
+}
+
+// GetUserTweetsStats return statistics about user tweets
+func (c *Client) GetUserTimeline(screenName string, after int64) ([]twitterGo.Tweet, *Limit, error) {
+	params := &twitterGo.UserTimelineParams{ScreenName: screenName, Count: 200}
+	if after != 0 {
+		params.MaxID = after
+	}
+	tweets, resp, err := c.client.Timelines.UserTimeline(params)
+	if err != nil {
+		return nil, nil, err
+	}
+	limits, err := GetLimits(resp)
+	return tweets, limits, err
 }
